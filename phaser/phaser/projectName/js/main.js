c28a2c3976b77a87b8f438bb99d005e5010ac679
Terry DuBois <tdubois@ucsc.edu>
2019-04-09 13:57:39 -0700

    Gold star

--- phaser/phaser/projectName/js/main.js
+++ phaser/phaser/projectName/js/main.js
@@ -1,14 +1,26 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(600, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-	// preload assets
+	game.load.image('sky', 'assets/sky.png');
+	game.load.image('ground', 'assets/platform.png');
+	game.load.image('star', 'assets/img/star.png');
+	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
+var platforms;
+
 function create() {
-	// place your assets
+	game.add.sprite(0, 0, 'star');
+
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+
+	game.add.sprite(0, 0, 'sky');
+
+	platforms.game.add.group();
+	platforms = game.add.group();
+	platforms.enableBody = true;
 }
 
 function update() {
-	// run game loop
 }
 
